b0371b6a104ad9223a5be80f8552c4d10f3f5ac0
Sheeri <awfief@gmail.com>
2015-08-03 09:33:05 -0400

    remove username/password, add in path and docs
    username/password are no longer appropriate for oauth2 as of April 2015. Furthermore, path and docs are hard-coded into the python code and should be in the config file instead, so I have made those changes.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 
 config = {
-    'USERNAME' : 'yourusername@gmail.com',
-    'PASSWORD' : 'yourpassword',
+    'PATH' : 'C:\\google-drive-data\\',
+    # all docs must be shared with client_email as defined in the json file for oauth2
+    'DOCS'     : [
+      {"doc":"My 1st doc title"},
+      {"doc":"My 2nd doc title"} 
+      ],
     }
